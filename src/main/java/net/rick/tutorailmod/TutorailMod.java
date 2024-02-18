70b732ba53f9e2b4e6fdd8d24d8e6d93db1f8fa8
Rick Lodewijk <ricklodewijk99@gmail.com>
2024-02-18 16:53:55 +0100

    Items, blocks werkend, sapphire_block dropt, alle ores zijn gemaakt, metal detector toegevoegd, sound block toegevoegd, fuel items

--- src/main/java/net/rick/tutorailmod/TutorailMod.java
+++ src/main/java/net/rick/tutorailmod/TutorailMod.java
@@ -2,6 +2,7 @@ package net.rick.tutorailmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -12,20 +13,26 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.rick.tutorailmod.block.ModBlocks;
+import net.rick.tutorailmod.item.ModCreativeModTabs;
 import net.rick.tutorailmod.item.ModItems;
 import org.slf4j.Logger;
 
 @Mod(TutorailMod.MOD_ID)
 public class TutorailMod
 {
-    public static final String MOD_ID = "tutorialmod";
+    public static final String MOD_ID = "tutorailmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorailMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
 
         modEventBus.addListener(this::commonSetup);
 
@@ -39,7 +46,10 @@ public class TutorailMod
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     @SubscribeEvent
---
+++ src/main/java/net/rick/tutorailmod/item/ModCreativeModTabs.java
@@ -0,0 +1,46 @@
+package net.rick.tutorailmod.item;
+
+import net.rick.tutorailmod.TutorailMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.rick.tutorailmod.block.ModBlocks;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorailMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORAIL_TAB = CREATIVE_MODE_TABS.register("tutorail_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorail_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+                        pOutput.accept(ModItems.STRAWBERRY.get());
+                        pOutput.accept(ModItems.PINE_CONE.get());
+
+                        pOutput.accept(Items.DIAMOND);
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
