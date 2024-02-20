09debccfe9f301059e9f7a78a83dcf72ce6fb708
Rick <ricklodewijk99@gmail.com>
2024-02-20 21:09:52 +0100

    food item detector and blocks and tooltips and de datagen

--- src/main/java/net/rick/tutorailmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/rick/tutorailmod/item/custom/MetalDetectorItem.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.rick.tutorailmod.util.ModTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -33,7 +34,7 @@ public class MetalDetectorItem extends Item {
             for(int i = 0; i <= positionClicked.getY() + 64; i++) {
                 BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
 
-                if(isValueBlock(state)) {
+                if(isValuableBlock(state)) {
                     outputValuableCoordinates(positionClicked.below(i), player, state.getBlock());
                     foundBlock = true;
 
@@ -63,7 +64,7 @@ public class MetalDetectorItem extends Item {
                     "( " + blockpos.getX() + ", " + blockpos.getY() + ", " + blockpos.getZ() + " )"));
     }
 
-    private boolean isValueBlock(BlockState state) {
-            return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+    private boolean isValuableBlock(BlockState state) {
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
